5a82106fbf3ff4b1594941b01dc7b2156c35078d
Shubham Kaura <shubhamkaura8@gmail.com>
2024-01-21 16:47:11 -0700

    Add Geolocation, map and render form

--- script.js
+++ script.js
@@ -10,3 +10,64 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+let map, mapEvent;
+
+navigator.geolocation.getCurrentPosition(function (position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coords = [latitude, longitude];
+
+  map = L.map('map').setView(coords, 13);
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  //Handling clicks on map
+  map.on(
+    'click',
+    function (mapE) {
+      mapEvent = mapE;
+      form.classList.remove('hidden');
+      inputDistance.focus();
+    },
+    function (error) {
+      alert('Something went wrong!');
+      console.log(error);
+    }
+  );
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    //Clear Input fields
+    inputDistance.value =
+      inputCadence.value =
+      inputDuration.value =
+      inputElevation.value =
+        '';
+
+    //Display marker
+    const { lat, lng } = mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('Workout')
+      .openPopup();
+  });
+});
+
+inputType.addEventListener('change', function (e) {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
