b872b06f2237e2e3e8010adda82310ec97ee414c
Shubham Kaura <shubhamkaura8@gmail.com>
2024-01-22 23:40:46 -0700

    Add setView functionality

--- script.js
+++ script.js
@@ -63,6 +63,7 @@ class Cycling extends Workout {
 //App
 class App {
   #map;
+  #mapZoomLevel = 13;
   #mapEvent;
   #workouts = [];
 
@@ -71,6 +72,7 @@ class App {
     this._getPosition();
     this._toggleElevationField();
     inputType.addEventListener('change', this._toggleElevationField);
+    containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
   }
 
   _getPosition() {
@@ -87,7 +89,7 @@ class App {
     const longitude = position.coords.longitude;
     const coords = [latitude, longitude];
 
-    this.#map = L.map('map').setView(coords, 13);
+    this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
@@ -238,6 +240,22 @@ class App {
           </div>`;
     }
     form.insertAdjacentHTML('afterend', html);
+  }
+
+  _moveToPopup(e) {
+    const workoutEl = e.target.closest('.workout');
+    if (!workoutEl) return;
+
+    const workout = this.#workouts.find(
+      work => work.id === workoutEl.dataset.id
+    );
+
+    this.#map.setView(workout.coords, this.#mapZoomLevel, {
+      animate: true,
+      pan: {
+        duration: 1,
+      },
+    });
   }
 }
 const app = new App();
